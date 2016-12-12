9ce81ff5432503d2c3dd7e6c890fa964d7f514fb
Jordan Lui <jordan.lui@gmail.com>
2016-12-11 20:38:36 -0800

    added data whitening. Some issues remain

--- svm.py
+++ svm.py
@@ -17,10 +17,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 #from sklearn import neighbors, datasets
 from sklearn import svm
+from sklearn import preprocessing
 import glob, os
 import csv
 import random
 import re
+import sys
+
+# File paths for accessing data
+path ='../Data/dec6_1'
+output_path = '../Analysis/'
+output_file = 'dec6.csv'
 
 # Function parameters
 cvalue = 2e-3
@@ -29,13 +36,10 @@ cvalue = 2e-3
 
 def xmatrix(files):
     # Accepts a list of files and builds x matrix and t matrix
-    # For loop
     x = []
     t = []
-#    print len(files)
     # Check if we have a single file input
     if isinstance(files,str) == True:
-#        print 'just one file'
         # Then just load the single file and output to x and t
         data = np.genfromtxt(files,delimiter=',')
         data = data[1:,:]
@@ -53,45 +57,77 @@ def xmatrix(files):
             
             # Remove the header row
             data = data[1:,:]
-    #        print data.shape
-            
-            # Get the label data, (gestures)
+            # Find the gesture number
             trial_info = re.findall('\[([0-9]{1,2})\]',file)
-        #    patient = trial_info[0]
             gesture = trial_info[1]
-        #    trialnumber = trial_info[2]    
-            
-        #    x_train = np.concatenate((x_train,data),axis=0)
+
             for row in data:
                 x.append(row)
                 t.append(gesture)
-        
+    # Reformat as arrays
     x = np.asarray(x)
     t = np.asarray(t)
     t = np.reshape(t,(len(t),1))
 
     return x,t
 
-path ='../Data/dec6_1'
-output_path = '../Analysis/'
-output_file = 'dec6.csv'
+def normtraintest(train,test):
+    # Normalizes a train and test matrix, in column by column fashion
+    # Scales each column values from 0 to 1. 
+    # Standardizes each column by subtracting mean and dividing by STDEV    
+    # Currently you must put two inputs. Can fix this later.
 
-# Get a list of files
+    # Loop through our data, one column at a time
+    for i in range(0,train.shape[1]):
+        coltrain = train[:,i]
+        coltest = test[:,i]
+        colmax = np.max(coltrain)
+        colmin = np.min(coltrain)
+        mean = np.mean(coltrain)
+        std = np.std(coltrain)
+        
+        # Standardize the data        
+        coltrain = (coltrain - mean)/std
+        coltest = (coltest - mean)/std        
+        
+        # Scale the data
+        # For some reason the model runs even worse if we both standardize and scale data. figure this out later
+#        coltrain = (coltrain - colmin)/(colmax - colmin)
+#        coltest = (coltest - colmin)/(colmax - colmin)
+        
+        
+        
+        # Put the values back into the matrix
+        
+        train[:,i] = coltrain
+        test[:,i] = coltest
+        
+    return train,test
+
+# Get a list of files 
 filelist = glob.glob(os.path.join(path,'*.csv'))
-
 numfiles = len(filelist)
 
-# Shuffle it and remove some for testing. Seed the shuffle.
+# Shuffle file list and remove some for testing. Seed the shuffle.
 random.seed(1)
 filelist = random.sample(filelist,len(filelist))
 
-segment = 0.15 # Let's train on 10% of the data
+segment = 0.15 # Percentage of data that we test on
 file_train = filelist[:numfiles-int(segment * numfiles)]
 file_test = filelist[-int(segment * numfiles):]
 
 # Generate x_train and x_test matrices
 x_train,t_train = xmatrix(file_train)
 x_test,t_test = xmatrix(file_test)
+
+# Cut out two columns since we aren't using the FSR Data
+#x_train = np.delete(x_train,[30,31],1)
+#x_test = np.delete(x_test,[30,31],1)
+
+# Data preprocessing
+# Normalize the data, column-wise
+x_train,x_test = normtraintest(x_train,x_test)
+
 
 # Create SVM Model
 
@@ -112,6 +148,10 @@ print 'overall accuracy is','{:04.2f}'.format(accuracy)
 accuracy = []
 for file in file_test:
     x1,t1 = xmatrix(file)
+    
+    # Temporary resizing of files. Remove
+#    x1 = np.delete(x1,[30,31],1)
+
     testdata = lin_clf.predict(x1)
     testdata = np.reshape(testdata,(len(testdata),1)) # reshape the data
 
@@ -129,7 +169,7 @@ print accuracy
 
 # Data plots
 plt.figure(1)
-plt.plot(x1[:,0:2],label='distance')
+#plt.plot(x1[:,0:2],label='distance')
 plt.plot(x1[:,-9:-4],label='fsr')
 plt.legend()
 plt.show()
