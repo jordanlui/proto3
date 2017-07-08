989d5037d0a18d43ba20fb080585742570af9628
Jordan Lui <jordan.lui@gmail.com>
2017-07-08 14:15:16 +0200

    not much

--- DataExtract.py
+++ DataExtract.py
@@ -2,6 +2,8 @@
 """
 Created on Sun Nov 27 13:22:19 2016
 
+Creates x matrix from CSV files.
+
 @author: Jordan
 """
 # Read all of the CSV files in the directory and extract the min, max, and average value per trial on two axis and outputs
