7395b3df9fbf165b622b9afd3cc94430afed243e
Jordan Lui <jordan.lui@gmail.com>
2017-10-01 16:04:26 -0700

    RegEx filtering to grab Full JSON Packets working, now need to save to file

--- analysis_bluetooth.py
+++ analysis_bluetooth.py
@@ -12,9 +12,23 @@ from __future__ import division
 import numpy as np
 import json
 from saveCSV import WriteProtoCSV
+import glob, os
+import fileinput
+import re
 
-path = "../data/sept29/"
+path = '../Data/sept29/'
 filename = 'move forward back.csv'
+
+# Find all files
+#os.chdir(path)
+filelist = []
+for file in glob.glob(path+'*.txt'):
+	if os.path.basename(file) == 'readme.txt':
+		# Do nothing, don't import this file
+		print 'readme.txt'
+	else:
+		filelist.append(file)
+
 
 def tryJSON(data):
 	try:
@@ -25,19 +39,34 @@ def tryJSON(data):
 		print data
 		return False
 
-data = []
+def csv2JSON(path,filename):
+	# Load a CSV file of JSON strings, load as proper JSON objects and save to file
+	
+	data = []
+	
+	# Load data and parse JSON
+	file = open(path+filename,"r")
+	for line in file:
+		rawline = line.strip('\n\r')
+		rawline = rawline.replace('""','"')
+		rawline = rawline[1:-1]
+		j = tryJSON(rawline)
+		data.append(j)
+	
+	# Now save to file, using a save CSV function
+	WriteProtoCSV(data,path,filename)
 
-# Load data and parse JSON
-file = open(path+filename,"r")
-for line in file:
-	rawline = line.strip('\n\r')
-	rawline = rawline.replace('""','"')
-	rawline = rawline[1:-1]
-	j = tryJSON(rawline)
-	data.append(j)
+# Loop
+textToSearch = "}.\n{"
+textToReplace = "}\n{"
 
-# Now save to file, using a save CSV function
-WriteProtoCSV(data,path,filename)
+# Reading from https://stackoverflow.com/questions/17140886/how-to-search-and-replace-text-in-a-file-using-python
 
+f = open(filelist[0],"r")
+filedata = f.read()
+f.close()
 
+# Regex Searching attempt
+query = "{[\s\S]+?}"
 
+m = re.findall(query,filedata) # Find all matching
